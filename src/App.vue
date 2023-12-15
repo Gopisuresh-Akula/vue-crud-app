228aa292a4eedfbc5110e0ea5f732b9e1baff93b
Gopisuresh-Akula <gopisureshakula123@gmail.com>
2023-12-15 10:16:20 +0530

    methods done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
-
 <template>
   <div>
     <!-- <basicComponent/> -->
     <global-component />
-    <LocalComponentVue/>
-    <My-Component msg="gopi"/>
+    <LocalComponentVue />
+    <My-Component msg="gopi" />
+    <VueMethodsVue  vue="...App.."/>
   </div>
   sss
 </template>
 <!-- MyComponent -->
 
 <script>
-import LocalComponentVue from './assets/Mocks/LocalComponent.vue'
-import MyComponent from './assets/Mocks/MyComponentStructureInfo.vue'
+import LocalComponentVue from "./assets/Mocks/LocalComponent.vue";
+import MyComponent from "./assets/Mocks/MyComponentStructureInfo.vue";
+import VueMethodsVue from "./assets/Mocks/VueMethods.vue";
 
 export default {
-  name:'App',
+  name: "App",
   components: {
-    "My-Component":MyComponent,
-  LocalComponentVue
-  }
-}
+    "My-Component": MyComponent,
+    LocalComponentVue,
+    VueMethodsVue: VueMethodsVue,
+  },
+};
 </script>
 
-<style >
-  
-</style>
+<style></style>
--- src/assets/Mocks/VueMethods.vue
+++ src/assets/Mocks/VueMethods.vue
@@ -1,28 +1,35 @@
-
 <!-- In Vue.js, you can use the @ symbol as shorthand for the v-on directive to h
     andle various events -->
 
-    <template>
-        <div>
-          <p>{{ message }}</p>
-          <p>{{ formatMessage('Hello, Vue!') }}</p>
-          <button @click="updateMessage">Update Message</button>
-        </div>
-      </template>
-      
-      <script>
-      export default {
-        name:'VueMethod',
-        data() {
-          return {
-            message: 'Hello, Vue!',
-          };
-        },
-        methods: {
-          updateMessage() {
-            this.message = 'New Message';
-          },
-          formatMessage(input) {
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <p>{{ asyncData }}</p>
+    <p>{{ formatMessage("Hello, Vue!") }}</p>
+    {{ vue }}
+    <button @click="updateMessage">Update Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueMethod",
+  data() {
+    return {
+      message: "Hello, Vue!",
+    };
+  },
+  props: {
+    vue: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    updateMessage() {
+      this.message = "New Message";
+    },
+    formatMessage(input) {
       return input.toUpperCase();
     },
     logMessage() {
@@ -33,20 +40,29 @@
       this.logMessage();
     },
     formattedMessage() {
+      console.log("mounted AGS");
       return this.message.toUpperCase();
+
     },
-          async fetchData() {
+    async fetchData() {
       // Simulate an API request
-      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos/1"
+      );
       const data = await response.json();
+      console.log("data..", data.title);
       this.asyncData = data.title;
     },
-        },
-        computed: {
+    
+  },
+  mounted() {
+    console.log("mounted AGS");
+    this.fetchData();
+  },
+  computed: {
     reversedMessage() {
-      return this.message.split('').reverse().join('');
+      return this.message.split("").reverse().join("");
     },
   },
-      };
-      </script>
-      
+};
+</script>
